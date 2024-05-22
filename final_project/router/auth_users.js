3a6825305edae958c5322b89485d77c1d8642752
soboli89 <soboli1989@gmail.com>
2024-05-22 13:23:13 -0400

    user log in

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,15 +14,49 @@ let users = [
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+const userMatches = users.filter((user) => user.username === username);
+return userMatches.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
+  console.log("login: ", req.query);
+  const username = req.query.username;
+  const password = req.query.password;
+
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+            accessToken,username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+
+
+
+
+
+
+
+
+  /*
    const {username, pwd} = req.body;
     if(username==="username" && pwd ==="password"){
         return res.json({
@@ -32,7 +66,7 @@ regd_users.post("/login", (req,res) => {
     return res
         .status(401)
         .json({message: "Invalid username and/or password"});
- // return res.status(300).json({message: "Yet to be implemented"});
+ // return res.status(300).json({message: "Yet to be implemented"});*/
 });
 
 // Add a book review
